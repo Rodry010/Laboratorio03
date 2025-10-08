d96a36646ec12ff506ad656d53fd25ddf05bf24f
Rodry010 <2024151048@estudantes.ips.pt>
2025-10-08 15:25:35 +0100

    Testes

---
+++ src/test/java/pt/pa/adts/TreeLinkedTest.java
@@ -0,0 +1,25 @@
+package pt.pa.adts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TreeLinkedTest {
+
+    @Test
+    void size() {
+        myTree.size()
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void isExternal() {
+    }
+
+    @Test
+    void isRoot() {
+    }
+}
