4324c2bb40d678716473c70241f8231be6b33702
DiogoSabino-git <202300149@estudantes.ips.pt>
2025-10-08 15:09:07 +0100

    Nível 1 - Criação da Árvore

--- src/main/java/pt/pa/Main.java
+++ src/main/java/pt/pa/Main.java
@@ -1,5 +1,8 @@
 package pt.pa;
 
+import pt.pa.adts.Position;
+import pt.pa.adts.TreeLinked;
+
 /**
  *
  * @author amfs
@@ -10,7 +13,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        TreeLinked<String> myTree = new TreeLinked<>("Ecosystem");
+        Position<String> root = myTree.root();
+        myTree.insert(root, "Anchovy");
+        Position<String> posTune = myTree.insert(root, "Tuna");
+        Position<String> posShark = myTree.insert(root, "Shark");
+        Position<String> posEagles = myTree.insert(root, "Eagles");
+        myTree.insert(posTune, "Mackerel");
+        Position<String> posBarracuda = myTree.insert(posTune, "Barracuda");
+        myTree.insert(posShark, "Dolphin");
+        myTree.insert(posEagles, "Snakes");
+        myTree.insert(posEagles, "Rabbits");
+        myTree.insert(posBarracuda, "Sardine");
 
+        System.out.print(myTree);
     }
 
 
