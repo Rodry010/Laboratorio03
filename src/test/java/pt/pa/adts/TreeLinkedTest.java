325c488f0bc61118e49d88806445e9012e60324d
Rodry010 <2024151048@estudantes.ips.pt>
2025-10-08 15:37:04 +0100

    Teste v2

--- src/test/java/pt/pa/adts/TreeLinkedTest.java
+++ src/test/java/pt/pa/adts/TreeLinkedTest.java
@@ -1,14 +1,33 @@
 package pt.pa.adts;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TreeLinkedTest {
 
+    @BeforeEach
+    void setup(){
+        TreeLinked<String> myTree = new TreeLinked<>("Ecosystem");
+        Position<String> root = myTree.root();
+        myTree.insert(root, "Anchovy");
+        Position<String> posTune = myTree.insert(root, "Tuna");
+        Position<String> posShark = myTree.insert(root, "Shark");
+        Position<String> posEagles = myTree.insert(root, "Eagles");
+        myTree.insert(posTune, "Mackerel");
+        Position<String> posBarracuda = myTree.insert(posTune, "Barracuda");
+        myTree.insert(posShark, "Dolphin");
+        myTree.insert(posEagles, "Snakes");
+        myTree.insert(posEagles, "Rabbits");
+        myTree.insert(posBarracuda, "Sardine");
+    }
+
+
+
     @Test
-    void size() {
-        myTree.size()
+    void size(){
+
     }
 
     @Test
